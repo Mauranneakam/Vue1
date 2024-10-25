49189597d24ea04a8bf05fee5d868275c5b01ff8
unknown <demaude19@gmail.com>
2024-10-25 05:20:50 +0100

    add SigninButton

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -4,48 +4,42 @@
     <BaseButton color="primary" :isDisabled="false">Primary Button</BaseButton>
     <BaseButton color="warn" :isDisabled="false">Warn Button</BaseButton>
     <BaseButton color="danger" :isDisabled="false">Danger Button</BaseButton>
-    <BaseButton color="danger" :isDisabled="true">Disabled Danger Button</BaseButton>
+    
     
     <!-- Button with increasing wait time -->
-    <BaseButton :isDisabled="isButtonDisabled" @click="handleClick">Click Me (Wait: {{ waitTime }}s)</BaseButton>
+    <AsyncButton :color="'primary'" :delay="clicks" @click="increaseClicks">
+      Cliquer pour ralentir ({{ clicks }} secondes d'attente) !
+    </AsyncButton>
+    <BaseButton color="primary" @click="signInWithMicrosoft">Sign In with Microsoft</BaseButton>
   </div>
 </template>
 
 <script>
 import BaseButton from './BaseButton.vue';
+import AsyncButton from './AsyncButyon.vue';
+import { signInAndGetUser } from '../lib/microsoftGraph';
 
 export default {
-  name: 'HomePage',
+  name: 'HomePage', 
+
   components: {
-    BaseButton
+    BaseButton, AsyncButton
   },
   data() {
     return {
-      isButtonDisabled: false,
-      clickCount: 0 // Counter for the number of clicks
+      clicks: 1 // Initialise le compteur à 1
     };
   },
-  computed: {
-    waitTime() {
-      // Calculate wait time based on the number of clicks
-      return this.clickCount + 1; // Start from 1 second
-    }
-  },
+  
   methods: {
-    handleClick() {
-      // Disable the button
-      this.isButtonDisabled = true;
-
-      // Increment click count
-      this.clickCount++;
-
-      // Create a promise that resolves after the wait time
-      return new Promise((resolve) => {
-        setTimeout(() => {
-          this.isButtonDisabled = false; // Re-enable the button
-          resolve(); // Resolve the promise
-        }, this.waitTime * 1000); // Convert to milliseconds
-      });
+   
+    async signInWithMicrosoft() {
+      const user = await signInAndGetUser(); // Appelle la fonction signInAndGetUser
+      if (user) {
+        alert(`Connexion réussie ! Bienvenue, ${user.name}`);
+      } else {
+        alert('Échec de la connexion');
+      }
     }
   }
 }
